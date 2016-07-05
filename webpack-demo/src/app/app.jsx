a2c7cbb4c2727f34456a573498e6e81a0c8fe7d8
dang4 <xsxuzhu@aliyun.com>
2016-07-05 10:01:39 +0800

    添加 webpack

---
+++ webpack-demo/src/app/app.jsx
@@ -0,0 +1,18 @@
+require("css/page.css");
+
+var React = require('react');
+var HelloComponent = React.createClass({
+  render:function(){
+    return (
+      <div>
+        <div className="logo"></div>
+        <h1>使用Webpack打包React</h1>
+        <p>React很好用</p>
+        <p>webpack棒棒的</p>
+        <p>react-hot-loader实现热插拔是很好的</p>
+      </div>
+    );
+  }
+});
+
+module.exports = HelloComponent;
---
+++ webpack-demo/src/main.jsx
@@ -0,0 +1,9 @@
+require("css/base.css");
+
+var React = require('react');
+
+var ReactDOM = require('react-dom');
+
+var HelloComponent = require('app/app');
+
+ReactDOM.render(<HelloComponent></HelloComponent>,document.getElementById('root'));
