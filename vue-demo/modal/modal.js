dbac97b3b082d3edb877b56084fb3fc159d396e5
dang4 <renwanchun66@163.com>
2017-01-14 16:06:39 +0800

    vue-modal

--- vue-demo/modal/modal.js
+++ vue-demo/modal/modal.js
@@ -1,15 +1,16 @@
 
-var ModalTrigger = function(Dialog){
+var ModalControl = function(){
     var modalEl = window.document.querySelector('.vue-modal');
     this.vm = new Vue({
         el:modalEl,
         template:'\
             <div :class="modalCls">\
-                <custom-dialog />\
+                <custom-dialog @close="dialogClose" :options="dialogOptions" />\
             </div>\
         ',
         data:{
-            active:false
+            active:false,
+            dialogOptions:{}
         },
         computed:{
             modalCls:function(){
@@ -20,33 +21,34 @@ var ModalTrigger = function(Dialog){
             }
         },
         components:{
-            'custom-dialog':Dialog
+            'custom-dialog':{template:'<div></div>'}
         },
         methods:{
-            show:function(Dialog){
+            show:function(Dialog,options){
                 this.$options.components['custom-dialog'] = Dialog;
                 this.active = true;
+                this.dialogOptions = options;
             },
-            hide:function(){
-                this.active = false;
+            dialogClose:function(){
+                this.close();
             },
             close:function(){
-
+                this.active = false;
+                this.$options.components['custom-dialog'] = '';
+                this.dialogOptions = {};
             }
         }
     });
 };
 
 
-ModalTrigger.prototype.show = function(){
-    this.vm.show();
+ModalControl.prototype.show = function(Dialog,options){
+    this.vm.close();
+    this.vm.show(Dialog,options);
 };
 
-ModalTrigger.prototype.hide = function(){
-    this.vm.hide();
+ModalControl.prototype.close = function(){
+    this.vm.close();
 };
 
-ModalTrigger.prototype.close = function(){
-    this.vm.$destroy();
-};
-
+var modalControl = new ModalControl();
