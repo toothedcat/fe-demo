f6579a5e12b4b7d22950c87154c3059cde879f17
toothedcat <toothedcat@163.com>
2017-09-25 16:53:16 +0800

    redux basics actions&reducers

---
+++ react-demo/redux-demo/src/reduces/index.js
@@ -0,0 +1,9 @@
+import {combineReducers} from 'redux';
+
+import visibilityFilters from './visibilityFilters';
+import todos  from './todos';
+
+const todoApp = combineReducers({
+    visibilityFilters,
+    todos
+});
